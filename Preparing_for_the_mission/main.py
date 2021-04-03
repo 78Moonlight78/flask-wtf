16a4271eb79464b8d25a81f0ad1892179f9a6ef9
Moonlight <44603620+78Moonlight78@users.noreply.github.com>
2021-04-03 11:53:24 +0700

    Update main.py

--- Preparing_for_the_mission/main.py
+++ Preparing_for_the_mission/main.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
-
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('index.html', title=title)
+    return render_template('base.html', title=title)
 
-
+    
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1', debug=True)
